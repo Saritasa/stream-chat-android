88e858017b057780427c256917bf1bee2500d31d
Maxim Maximov <maxmaxandr@gmail.com>
2020-05-20 16:48:42 +0700

    Start query from position implementation

--- library/src/main/java/com/getstream/sdk/chat/viewmodel/ChannelViewModelFactory.java
+++ library/src/main/java/com/getstream/sdk/chat/viewmodel/ChannelViewModelFactory.java
@@ -11,15 +11,17 @@ public class ChannelViewModelFactory implements ViewModelProvider.Factory {
     private Application app;
     private final String channelType;
     private final String channelId;
+    private String messageId;
 
-    public ChannelViewModelFactory(Application application, String channelType, String channelId) {
+    public ChannelViewModelFactory(Application application, String channelType, String channelId, String messageId) {
         this.app = application;
         this.channelType = channelType;
         this.channelId = channelId;
+        this.messageId = messageId;
     }
 
     @Override
     public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
-        return (T) new ChannelViewModel(app, channelType, channelId);
+        return (T) new ChannelViewModel(app, channelType, channelId, messageId);
     }
 }
